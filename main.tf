1752bfd5d84f21141bf77866f022f4609adae4a5
aj-ali <71270338+aj-ali@users.noreply.github.com>
2020-09-29 17:16:25 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Replace this with your bucket name!
-    bucket         = "terraform-up-and-running-state"
+    bucket         = "terraform-state2222"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-west-2"
     # Replace this with your DynamoDB table name!
