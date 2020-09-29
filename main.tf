3cc72e0b0ed37c5b40be8f3f0d13d5bc0218a5eb
aj-ali <71270338+aj-ali@users.noreply.github.com>
2020-09-29 14:23:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,32 +3,20 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "../../"
+  source = "terraform-aws-modules/vpc/aws"
 
-  name = "bjss-test-vpc"
-
+  name = "my-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-2a", "eu-west-2b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-
-  enable_ipv6 = true
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  single_nat_gateway = true
-
-  public_subnet_tags = {
-    Name = "overridden-name-public"
-  }
+  enable_vpn_gateway = true
 
   tags = {
-    Owner       = "user"
+    Terraform = "true"
     Environment = "dev"
   }
-
-  vpc_tags = {
-    Name = "vpc-name"
-  }
 }
-
