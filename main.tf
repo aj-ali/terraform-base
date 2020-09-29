a2d3129e02b121c70094cd624849fc0f6ab4b6e7
AJ <ajmal.ali@bjss.com>
2020-09-29 14:18:13 +0100

    added simple vpc

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+module "vpc" {
+  source = "../../"
+
+  name = "bjss-test-vpc"
+
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+
+  enable_ipv6 = true
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  public_subnet_tags = {
+    Name = "overridden-name-public"
+  }
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  vpc_tags = {
+    Name = "vpc-name"
+  }
+}
+
